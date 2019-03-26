21ff1392d147995bab8b1435f656a7b93e87aa4d
Morten Lied Johansen <mortenjo@ifi.uio.no>
2019-03-26 22:05:58 +0100

    Be more informative

--- dockerma/__init__.py
+++ dockerma/__init__.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8
 
+from pkg_resources import get_distribution, DistributionNotFound
+
 
 def main():
-    print("Hello world")
+    try:
+        dist = get_distribution("dockerma")
+        description = "{} {}".format(dist.project_name, dist.version)
+    except DistributionNotFound:
+        description = "dockerma <unknown>"
+    print("Hello, this is {}".format(description))
